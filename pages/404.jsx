0a61a29bd4349981c5bfbcb1157e442357d0112c
🎈 <numofran6@gmail.com>
2022-12-20 17:28:39 +0000

    0/10

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function PageNotFound() {
 	return (
-		<div className="h-screen flex flex-col items-center justify-center space-y-5">
-			<h1 className="text-3xl text-emerald-200">
+		<div className="h-screen flex flex-col items-center justify-center space-y-5 text-center">
+			<h1 className="text-2xl text-emerald-200">
 				404 | This page could not be found.
 			</h1>
 
