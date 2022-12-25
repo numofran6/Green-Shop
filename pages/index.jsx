52a790e8decee170a81f6dba30722f088534cd02
🎈 <numofran6@gmail.com>
2022-12-25 13:41:48 +0000

    0/10

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,40 +7,6 @@ import { motion } from 'framer-motion';
 export default function Home({ products }) {
 	return (
 		<>
-			<Head>
-				<meta charset="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="theme-color" content="#000000" />
-				<meta
-					name="description"
-					content="E-Commerce App. Project of Francis Numo. A Front-End Developer who specializes in building elegant user interface with the aim to create an exceptional digital experience"
-				/>
-				<meta
-					name="keywords"
-					content="Frontend Developer, Mobile Developer, Open Source, Freelance Web Developer, Freelance Mobile Developer, React Native Developer, Developer Portfolio, React Developer, Web Developer, React Frontend Developer"
-				/>
-
-				<meta name="description" content="Page description" />
-				<meta property="og:title" content="Green Shop" />
-				<meta
-					property="og:description"
-					content="E-Commerce App. Project of Francis Numo. A Front-End Developer who specializes in building elegant user interface with the aim to create exceptional digital experience"
-				/>
-				<meta property="og:image" content="../public/images/shirt1.jpg" />
-
-				<meta property="og:image:alt" content="Green Shop" />
-				<meta property="og:locale" content="en_GB" />
-				<meta property="og:type" content="website" />
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta
-					property="og:url"
-					content="https://www.greenshopping.vercel.app/"
-				/>
-				<meta name="twitter:card" content="summary" />
-				<meta name="theme-color" content="#000000" />
-				<meta name="robots" content="index, follow" />
-			</Head>
-
 			<motion.div
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
